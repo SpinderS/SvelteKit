955458c28e7e82b7014abfc29d3f2f5f77a0e639
SSpinder <165291773+SpinderS@users.noreply.github.com>
2025-04-07 15:56:57 +0200

    Src files of SvelteKit assignment

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<h1>About</h1>
+<p>
+	<a
+		href="/home"
+		class="text-gray-900 bg-indigo-400 rounded-3xl border-2 border-indigo-500 mx-2 px-6 font-mono text-2xl font-semibold cursor-pointer duration-200
+hover:bg-indigo-300 hover:shadow-xl hover:border-indigo-300 hover:scale-105">home</a
+	>
+	<a
+		href="/about"
+		class="text-gray-900 bg-indigo-400 rounded-3xl border-2 border-indigo-500 mx-2 px-6 font-mono text-2xl font-semibold cursor-pointer duration-200 hover:bg-indigo-300 hover:border-indigo-300
+"
+		>about
+	</a>
+</p>
